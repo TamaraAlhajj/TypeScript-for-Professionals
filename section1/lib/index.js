f36f37c6003b720852b2be159d4863605a96edc4
TamaraKunnanath <tamara.kunnanath@gmail.com>
2022-10-30 20:58:40 -0400

    Generics and any vs unknown

--- section1/lib/index.js
+++ section1/lib/index.js
@@ -1,18 +1,104 @@
 "use strict";
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+Object.defineProperty(exports, "__esModule", { value: true });
 let msg = 'Hello world';
 msg += ' again';
 console.log(msg);
 // Primitives
-let isPresent = false;
-let magic = 66.6;
-let hello = 'world';
-let notDefined = undefined;
-let notPresent = null;
-let penta = Symbol('star');
-let big = 24n;
+const isPresent = false;
+const magic = 66.6;
+const hello = 'world';
+let notDefined;
+const notPresent = null;
+const penta = Symbol('star');
+const big = 24n;
 // Instance Types
-let regexp = new RegExp('ab+c');
-let array = [1, 2, 3];
-let set = new Set(array);
+const regexp = /ab+c/;
+const array = [1, 2, 3];
+const set = new Set(array);
 class Queue {
+    constructor() {
+        this.data = [];
+    }
+    push(item) { this.data.push(item); }
+    pop() { return this.data.shift(); }
 }
+const queue = new Queue();
+// Arrays
+let arr = [1, 2, 3];
+arr = [1];
+arr = [1, 2, 3, 4, 5, 6];
+// Type error example: arr = ['hello']
+// Tuples
+let tuple1;
+let tuple2;
+tuple1 = [1, '2'];
+tuple2 = [1, 2];
+tuple1 = [4, 'random'];
+tuple2 = [0, 0];
+const center = {
+    x: 0,
+    y: 0
+};
+const unit = {
+    x: 1,
+    y: 1
+};
+// TS allows modification of const variable properties
+unit.x = 5;
+center.y = 5;
+// Functions
+function add(a, b) {
+    return a + b;
+}
+function log(message) {
+    console.log('LOG: ', message);
+}
+// handling rest params
+function sum(...values) {
+    return values.reduce((previous, current) => {
+        return previous + current;
+    });
+}
+sum(1, 2, 3, 4, 5);
+let getSum;
+getSum = (a, b) => a + b;
+getSum = (a, b, c) => a + b + c;
+const user = { id: 'user-a123' };
+const product = { id: 'product-a123' };
+let pointA = { x: 0, y: 0 };
+const pointB = { x: 0, y: 0, z: 0 };
+// TS allows for extra info, in assignment function params, etc
+// Called Duck-Typing (if it walks like a duck...)
+// Of course this doesn't work the other way around, because of missing info
+pointA = pointB;
+// Classes
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    catch(numOfMice) {
+        console.log(`${this.name} caught ${numOfMice} mice!`);
+    }
+}
+const cat = new Animal('Mochi');
+cat.catch(10);
+// Generics
+const numFIFO = new Queue();
+queue.push(123);
+queue.push('123');
+queue.pop();
+numFIFO.push(123);
+numFIFO.push(22);
+// ERR: numFIFO.push('123')
+numFIFO.pop();
+// any vs. unknown
+// universal super types
+// any var can be assigned them
+// any allows you completely bypass the type system (created for easy migration from js to ts)
+// can be used as a boolean even if it's a string
+// this decreases type safety
+// unknown is different and safer, you can accept anything into an unknown but will not allow unsafe usage
+// must match usage type definition
+// This can be used as a universal utility, such as when you have a function that must accept any type
--- section1/src/index.ts
+++ section1/src/index.ts
@@ -29,7 +29,7 @@ class Queue<T> {
   pop (): T | undefined { return this.data.shift() }
 }
 
-const queue: Queue<number> = new Queue()
+const queue = new Queue()
 
 // Arrays
 let arr: number[] = [1, 2, 3]
@@ -94,3 +94,63 @@ getSum = (a: number, b: number) => a + b
 getSum = (a: number, b: number, c: number) => a + b + c
 
 // Structural Typing
+type User = { id: string }
+type Product = { id: string }
+
+const user: User = { id: 'user-a123' }
+const product: Product = { id: 'product-a123' }
+
+type Point2D = { x: number, y: number }
+type Point3D = { x: number, y: number, z: number }
+
+let pointA: Point2D = { x: 0, y: 0 }
+const pointB: Point3D = { x: 0, y: 0, z: 0 }
+
+// TS allows for extra info, in assignment function params, etc
+// Called Duck-Typing (if it walks like a duck...)
+// Of course this doesn't work the other way around, because of missing info
+pointA = pointB
+
+// Classes
+class Animal {
+  // private is accessible only within the class
+  // protected is accessible from within children as well
+  // In both cases attribute is not accessible by the object eg. cat.name
+  private readonly name: string
+
+  constructor (name: string) {
+    this.name = name
+  }
+
+  catch (numOfMice: number): void {
+    console.log(`${this.name} caught ${numOfMice} mice!`)
+  }
+}
+
+const cat = new Animal('Mochi')
+cat.catch(10)
+
+// Generics
+
+const numFIFO = new Queue<number>()
+
+queue.push(123)
+queue.push('123')
+queue.pop()
+
+numFIFO.push(123)
+numFIFO.push(22)
+// ERR: numFIFO.push('123')
+numFIFO.pop()
+
+// any vs. unknown
+// universal super types
+// any var can be assigned them
+
+// any allows you completely bypass the type system (created for easy migration from js to ts)
+// can be used as a boolean even if it's a string
+// this decreases type safety
+
+// unknown is different and safer, you can accept anything into an unknown but will not allow unsafe usage
+// must match usage type definition
+// This can be used as a universal utility, such as when you have a function that must accept any type
